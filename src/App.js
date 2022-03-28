c3c2e419d2c3afec03794a6a796dee764a35e96a
shubhamjoshi99899 <shubhamjoshi99899@gmail.com>
2022-03-28 12:55:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Calculator from './components/calculator/Calculator';
+import Header from './components/Header';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Calculator/>
+            <footer>
+             <p style={{textAlign:'center', marginTop:'5rem', color:'black'}}>Copyright © 2022 Shubham Joshi</p>
+         </footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <header>
+		
+		<h1 class="header-title" style={{textAlign:'center', marginBottom:'10px'}}>Calculator App</h1>
+	</header>
+    </div>
+  )
+}
+
+export default Header
